0a4e0431cd359c9bedc424b2634d2740e289b96e
Gabriel Malet <gmalet@galilee.fr>
2015-07-20 16:47:22 +0200

    mysqldump of schema

---
+++ app/fixtures/feed.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: api_export
+-- ------------------------------------------------------
+-- Server version	5.5.43-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `feed_type`
+--
+
+DROP TABLE IF EXISTS `feed_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feed_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) DEFAULT NULL,
+  `is_enabled` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`id`),
+  INDEX (`is_enabled`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feed_type`
+--
+
+LOCK TABLES `feed_type` WRITE;
+/*!40000 ALTER TABLE `feed_type` DISABLE KEYS */;
+INSERT INTO `feed_type` VALUES (1,'reddit',''),(2,'other','');
+/*!40000 ALTER TABLE `feed_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feed`
+--
+
+DROP TABLE IF EXISTS `feed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) DEFAULT NULL,
+  `url` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_enabled` bit(1) NOT NULL DEFAULT b'0', 
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`id`),
+  KEY `type` (`type`),
+  INDEX (`type`, `update_date`, `is_enabled`),
+  CONSTRAINT `feed_ibfk_1` FOREIGN KEY (`type`) REFERENCES `feed_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `feed_item`
+--
+
+DROP TABLE IF EXISTS `feed_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feed_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feed_id` int(11) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `categories` varchar(255) DEFAULT NULL,
+  `author_name` varchar(255) DEFAULT NULL,
+  `author_uri` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `resume` text,
+  `extract` text,
+  `is_enabled` bit(1) NOT NULL DEFAULT b'0',
+  `is_viewed` bit(1) NOT NULL DEFAULT b'0',
+  `is_approved` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`id`),
+  UNIQUE KEY `unique_hash` (`hash`),
+  KEY `feed_id` (`feed_id`),
+  INDEX (`hash`, `feed_id`, `is_enabled`, `is_viewed`, `is_approved`, `update_date`),
+  CONSTRAINT `feed_item_ibfk_1` FOREIGN KEY (`feed_id`) REFERENCES `feed` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feed_item`
+--
+
+LOCK TABLES `feed_item` WRITE;
+/*!40000 ALTER TABLE `feed_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feed_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-07-20 16:25:06
