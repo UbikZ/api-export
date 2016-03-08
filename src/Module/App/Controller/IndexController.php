264580b99585134290339b8bdfb634be59958793
Gabriel Malet <gabrielmalet@gmail.com>
2016-03-08 18:37:17 +0100

    Fix

--- src/Module/App/Controller/IndexController.php
+++ src/Module/App/Controller/IndexController.php
@@ -24,7 +24,7 @@ class IndexController extends AbstractController
     public function viewAction(Request $request)
     {
       if (!$request->get('startDate')) {
-        throw new Exception("Use 'startDate' parameter to scale your search.");
+        throw new \Exception("Use 'startDate' parameter to scale your search.");
       }
       $filterFeed = new DTO\Filter\FeedItem();
       $filterFeed->id = $request->get('id');
