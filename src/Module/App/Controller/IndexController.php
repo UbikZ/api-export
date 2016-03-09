a84ee8f9ca3b781ccb0ef2ac8957f8f7a81c4e32
Gabriel Malet <gabrielmalet@gmail.com>
2016-03-09 10:00:38 +0100

    Fix

--- src/Module/App/Controller/IndexController.php
+++ src/Module/App/Controller/IndexController.php
@@ -29,7 +29,8 @@ class IndexController extends AbstractController
       $filterFeed->isApproved = $request->get('approved');
       $filterFeed->isReposted = $request->get('reposted');
       $filterFeed->isViewed = $request->get('viewed');
-      $filterFeed->startDate = new \DateTime();
+      $filterFeed->startDate = $request->get('startDate') ? new \DateTime($request->get('startDate')) : new DateTime();
+      $filterFeed->endDate = $request->get('endDate') ? new \DateTime($request->get('endDate')) : null;
       $feeds = Manager\FeedItem::get($filterFeed, null, true);
 
       return $this->render(['feeds' => $feeds]);
