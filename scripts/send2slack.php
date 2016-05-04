cc1015b8bdbdacbfd674a523fbba18dad253439e
Gabriel Malet <gabrielmalet@gmail.com>
2016-05-05 00:46:49 +0200

    Avoid pushing some stuff if no links

--- scripts/send2slack.php
+++ scripts/send2slack.php
@@ -18,16 +18,19 @@ function main($argc, $argv = array())
         ];
         $apiUrl = $argv[1] . '/feed-item?approved=1&sent=0';
         $feeds = json_decode(file_get_contents($apiUrl), true);
-        $text = 'Plop bitches (*' . count($feeds) . ' links*)' . PHP_EOL;
-        foreach ($feeds as $feed) {
-            $id = $feed['id'];
-            $text .= $feed['url'] . ' ' . $feed['comment'] . PHP_EOL;
-            exec("curl --data \"id=$id&sent=1\" $argv[1]/update-item");
+        $count = count($feeds),
+        $text = 'Plop bitches (*' . $count . ' links*)' . PHP_EOL;
+        if ($count > 0) {
+            foreach ($feeds as $feed) {
+                $id = $feed['id'];
+                $text .= $feed['url'] . ' ' . $feed['comment'] . PHP_EOL;
+                exec("curl --data \"id=$id&sent=1\" $argv[1]/update-item");
+            }
+            $payload['text'] .= $text;
+            $sendJson = json_encode($payload);
+            var_dump($payload);
+            exec("curl -X POST --data-urlencode 'payload=$sendJson' $argv[4]");
         }
-        $payload['text'] .= $text;
-        $sendJson = json_encode($payload);
-        var_dump($payload);
-        exec("curl -X POST --data-urlencode 'payload=$sendJson' $argv[4]");
     } catch (\Exception $e) {
         echo $e->getMessage() . PHP_EOL;
         die;
